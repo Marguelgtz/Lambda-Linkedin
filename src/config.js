6e6fea5f0f75f3a72fde73539ae9460d4c629277
Lambda_School_Loaner_26 <marguel.gtz@gmail.com>
2018-10-20 19:55:21 -0500

    added apiKey and spreadsheetId to config file

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export default {
+    apiKey: "AIzaSyBExKK-aDXbTLA0xCF8zwA2O-Q5J5nzPxQ",
+    discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+    spreadsheetId: "1eOEQukWHGv7zkrucYLU6aYJM8bhJ_nHfs2RwQunF-rE"
+};
